4e85485ad4a279453dd24f3b3feb3bfd495136ed
Guillaume Corlouer <guillaume.corlouer@gmail.com>
2019-03-15 16:54:06 +0000

    modif

---
+++ descriptive_stat.m
@@ -0,0 +1,18 @@
+%% Descriptive statistics of some data
+X=double(EEG.data); 
+av_X=mean(X');
+var_X=var(X');
+min_X=min(abs(X),[],2);
+max_X=max(abs(X),[],2);
+ax1=subplot(4,1,1);
+bar(av_X)
+title('Sample mean')
+ax2=subplot(4,1,2);
+bar(var_X)
+title('Variance')
+ax3=subplot(4,1,3);
+bar(min_X)
+title('Absolute minimium')
+ax4=subplot(4,1,4);
+bar(max_X)
+title('Absolute maximum')
---
+++ mni2AAL.m
@@ -0,0 +1,82 @@
+function [label,number,index] = mni2AAL(mni)
+
+% this function converts MNI coordinate to a description of brain structure
+% in AAL.
+%
+%   mni: the coordinates (MNI) of some points, in mm.  It is Nx3 matrix
+%   where each row is the coordinate for one point
+%
+%   Example:
+%
+%   [label,number,index] = mni2AAL([72 -34 -2; 50 22 0])
+%
+% Adapted from 'cuixuFindStructure.m', Xu Cui (2007): http://www.alivelearn.net/?p=14
+%
+% AALDB.mat from xjView: http://www.alivelearn.net/xjview/ file TDdatabase.mat
+
+load('AALDB.mat'); % AALDB.mat must be on your PATH
+
+N = size(mni,1);
+
+% round the coordinates
+mni = round(mni/2)*2;
+
+T = [...
+     2     0     0   -92
+     0     2     0  -128
+     0     0     2   -74
+     0     0     0     1];
+
+label  = cell(N,1);
+number = zeros(N,1);
+index  = mni2cor(mni,T);
+
+for i = 1:N
+	graylevel = AAL.mnilist(index(i,1),index(i,2),index(i,3));
+
+	if graylevel == 0
+		label{i} = 'undefined';
+	else
+		label{i} = AAL.anatomy{graylevel};
+	end
+
+	number(i) = graylevel;
+end
+
+function coordinate = mni2cor(mni,T)
+
+% function coordinate = mni2cor(mni, T)
+% convert mni coordinate to matrix coordinate
+%
+% mni: a Nx3 matrix of mni coordinate
+% T: (optional) transform matrix
+% coordinate is the returned coordinate in matrix
+%
+% caution: if T is not specified, we use:
+% T = ...
+%     [-4     0     0   84;...
+%      0     4     0  -116;...
+%      0     0     4   -56;...
+%      0     0     0     1];
+%
+% xu cui
+% 2004-8-18
+
+if isempty(mni)
+    coordinate = [];
+    return;
+end
+
+if nargin == 1
+	T = ...
+        [-4     0     0   84;...
+         0     4     0  -116;...
+         0     0     4   -56;...
+         0     0     0     1];
+end
+
+coordinate = [mni(:,1) mni(:,2) mni(:,3) ones(size(mni,1),1)]*(inv(T))';
+coordinate(:,4) = [];
+coordinate = round(coordinate);
+
+return;
