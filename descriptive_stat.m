c08c136fc2f31612a52c63e93b6566be3c99412e
Guillaume Corlouer <guillaume.corlouer@gmail.com>
2019-04-26 14:58:35 +0100

    update corr

--- descriptive_stat.m
+++ descriptive_stat.m
@@ -17,12 +17,39 @@ window_size=10000;
 num_chan=size(tsdata_ROI,1);
 tsdata_length=size(tsdata_ROI,2);
 tsdata_slided=tsdata2slided(tsdata_ROI, window_size,num_chan,tsdata_length);
+%% Correlation 
+[corr_pears,pval_corr]=corr(tsdata_slided(:,:,2)');
+imagesc(corr_pears);
+colormap(jet);
+colorbar;
+title('Partial correlation AnRa, rest, good chans ')
+%% Correlation significance 
+%  Want alpha very small to reduce false positive.
+%  Higher false negative is ok, we want to get rid of highly partially
+%  correlated channels to keep more representative one 
+alpha=0.0005; 
+sig_corr=significance(pval_corr,alpha,'Bonferroni');
+imagesc(sig_corr);
+colormap(jet);
+colorbar;
+title('Significance of correlation AnRa, rest')
 %% Partial correlation 
-pcorr=partialcorr(tsdata_slided(:,:,1)');
+[pcorr,pval_pcorr]=partialcorr(tsdata_slided(:,:,1)');
 imagesc(pcorr);
 colormap(jet);
 colorbar;
 title('Partial correlation AnRa, rest')
+%set(gca,'XTick',[1:2],'XTickLabel',{'poz','fp1'})
+%% Significance of partial correlation 
+%  Want alpha very small to reduce false positive.
+%  Higher false negative is ok, we want to get rid of highly partially
+%  correlated channels to keep more representative one 
+alpha=0.0005; 
+sig_pcorr=significance(pval_pcorr,alpha,'Bonferroni');
+imagesc(sig_pcorr);
+colormap(jet);
+colorbar;
+title('Significance of partial correlation AnRa, rest')
 %% Partial Correlation in ROIs
 pick_ROI=6;
 pick_chan_ROI=find(chan2ROIidx==pick_ROI);
@@ -35,12 +62,12 @@ imagesc(pcorr(good_ch_idx,good_ch_idx));
 colormap(jet);
 colorbar;
 title(['Partial correlation AnRa, rest, ROI ', num2str(pick_ROI)])
-%% Get rid off channels with high enough correlation
-drop_chan=[]
+%% Get rid off channels with significant partial correlation
+drop_chan=[];
 for i=1:size(pcorr,1)
     for j=i+1:size(pcorr,1)
-        if pcorr(i,j)>= 0.8
-            drop_chan=horzcat(drop_chan,pick_chan(j))
+        if pcorr(i,j)>= 0.5
+            drop_chan=horzcat(drop_chan,pick_chan(i));
         else 
             j=j+1;
         end
